17d78979128fa20e619c60afaedbe9251c3167b7
eduardo-raniero <eduardoraniero@gmail.com>
2022-02-10 21:24:46 -0300

    home-banner

---
+++ components/Menu/index.jsx
@@ -0,0 +1,9 @@
+import styles from './menu.module.scss';
+
+export default function Menu(){
+    return(
+        <div className={styles.box}>
+            <p><strong>E</strong>duardo<strong>R</strong>aniero<strong>S</strong>ilva</p>
+        </div>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,22 @@
+
+//CSS
+import styles from '../styles/Home.module.scss'
+
+//REACT-ICONS
+import { FaReact } from 'react-icons/fa';
+import { SiNextdotjs, SiMaterialui } from 'react-icons/si';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <article className={styles.banner}>
+        <h1>Sou um Dev. Front-End especializado em:</h1>
+        <div className={styles.stacksBox}>
+          <h2 style={{color: '#00D8FF'}}><FaReact /></h2>
+          <h2 style={{color: '#fff'}}><SiNextdotjs /></h2>
+          <h2 style={{color: '#0081CB'}}><SiMaterialui /></h2>
+        </div>
+      </article>
+    </div>
+  )
+}
